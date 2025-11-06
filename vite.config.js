836fd43e16536062012ec5758d85e319b9020c96
ChiuYinchi <h24096118@gs.ncku.edu.tw>
2025-11-06 10:25:11 +0800

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -18,3 +19,25 @@ export default defineConfig({
   }
 })
 
+=======
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+  base: '/lotus-bf-frontend-YINCHI320/',
+
+  plugins: [react()],
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    allowedHosts: ['mil.psy.ntu.edu.tw']
+  }
+})
+
+>>>>>>> 1342622 (initial commit)
